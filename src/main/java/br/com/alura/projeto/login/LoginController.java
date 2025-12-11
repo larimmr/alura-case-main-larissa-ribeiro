c1578773d2121286d1088c3d6b7335232df3ad55
Larissa Ribeiro <larissa_mmr@hotmail.com>
2025-12-11 11:12:46 -0300

    feat: enhance Category and LoginController to support course listing by category in login view

--- src/main/java/br/com/alura/projeto/login/LoginController.java
+++ src/main/java/br/com/alura/projeto/login/LoginController.java
@@ -4,12 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.alura.projeto.category.CategoryRepository;
+import br.com.alura.projeto.course.CourseRepository;
+
 @Controller
 public class LoginController {
 
+    private final CourseRepository courseRepository;
+    private final CategoryRepository categoryRepository;
+
+    public LoginController(CourseRepository courseRepository, CategoryRepository categoryRepository) {
+        this.courseRepository = courseRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping("/")
     public String home(Model model) {
-        //TODO: Implementar a Questão 3 - Front-End da Página de Login aqui...
+      
+        var categories = categoryRepository.findAll();
+        categories.forEach(cat -> {
+            cat.setCourses(courseRepository.findByCategoryId(cat.getId()));
+        });
+        model.addAttribute("categories", categories);
 
         return "login";
     }
