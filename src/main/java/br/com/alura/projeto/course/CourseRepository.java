6a383eab018c3c53bc337371c2804221b2d4590e
Larissa Ribeiro <larissa_mmr@hotmail.com>
2025-12-11 11:09:40 -0300

    feat: add method to find courses by category ID in CourseRepository

--- src/main/java/br/com/alura/projeto/course/CourseRepository.java
+++ src/main/java/br/com/alura/projeto/course/CourseRepository.java
@@ -2,10 +2,14 @@ package br.com.alura.projeto.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     boolean existsByCode(String code);
 
     Optional<Course> findByCode(String code);
+
+    List<Course> findByCategoryId(Long categoryId);
+
 }
